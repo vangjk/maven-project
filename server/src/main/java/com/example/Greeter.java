e6f0712fbf72535b005b34d035b7c2dae046c0a2
EC2 Default User <ec2-user@ip-172-31-41-151.us-east-2.compute.internal>
2018-04-22 16:53:36 +0000

    more style fixes

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,8 +13,8 @@ public class Greeter {
   }
   
   /**
-   * @param someone the name of a person
-   * @return greeting string
+   * @param someone the name of a person.
+   * @return greeting string.
    */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
